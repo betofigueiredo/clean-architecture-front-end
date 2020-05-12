8358fbc35d2bd5f1b58a3f10815679219699c765
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 09:48:33 -0300

    [master] feat: Clean reducer

--- 1 - modules with entities and use-cases/src/domains/Items/store/reducers.js
+++ 1 - modules with entities and use-cases/src/domains/Items/store/reducers.js
@@ -1,26 +1,12 @@
-import usuario from './store';
+import items from './store';
 import reducerUtil from '../../../utils/redux/reducerUtil';
 
-// Actions external logic
-import loginReceived from './logic/loginReceived';
-import loginReceivedFailed from './logic/loginReceivedFailed';
-import loginRequested from './logic/loginRequested';
-import authRequested from './logic/authRequested';
-import authReceived from './logic/authReceived';
-import authRequestedFailed from './logic/authRequestedFailed';
-import checkExpiredToken from './logic/checkExpiredToken';
-import authCleared from './logic/authCleared';
+// Use cases
+import addItem from '../use_cases/addItem';
 
 const funcs = {
-	'[auth] LOGIN_RECEIVED': loginReceived,
-	'[auth] LOGIN_REQUEST_FAILED': loginReceivedFailed,
-	'[auth] LOGIN_REQUESTED': loginRequested,
-	'[auth] AUTH_REQUESTED': authRequested,
-	'[auth] AUTH_RECEIVED': authReceived,
-	'[auth] AUTH_REQUEST_FAILED': authRequestedFailed,
-	'[usuario] CHECK_EXPIRED_TOKEN': checkExpiredToken,
-	'[usuario] AUTH_CLEARED': authCleared,
+	'[items] ADD_ITEM': addItem,
 };
 
-const reducer = reducerUtil(usuario, funcs);
+const reducer = reducerUtil(items, funcs);
 export default reducer;
