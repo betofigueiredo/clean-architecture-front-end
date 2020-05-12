666b1770387a80030b37889801d5b5d6cc7b96dd
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 15:38:22 -0300

    [master] feat: Comment saga

--- 1 - modules with entities and use-cases/src/utils/redux/combineSagas.js
+++ 1 - modules with entities and use-cases/src/utils/redux/combineSagas.js
@@ -1,9 +1,9 @@
-import { all, fork } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 
-import itemsSaga from '../../domains/Items/store/sagas';
+// import itemsSaga from '../../domains/Items/store/sagas';
 
 export default function* rootSaga() {
 	yield all([
-		fork(itemsSaga),
+		// fork(itemsSaga),
 	]);
 }
