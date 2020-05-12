ada083df7edeff971da721623dfdc91e560b24c8
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 09:46:55 -0300

    [master] feat: Clean routes

--- 1 - modules with entities and use-cases/src/routes.js
+++ 1 - modules with entities and use-cases/src/routes.js
@@ -1,27 +1,15 @@
-/* eslint-disable */
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-// Loading
-// import LoadingPage from './components/LoadingPage';
-import NotFound404 from './modules/shared/pages/NotFound404';
-
 // Pages
-// const Equipes = lazy(() => import('./modules/Equipes/pages/Equipes'));
-import Team from './modules/Teams/pages/Team';
-import Teams from './modules/Teams/pages/Teams';
-import EmployeeProfile from './modules/Employees/pages/EmployeeProfile';
+import Items from './domains/Items/pages/Items';
 
 const Routes = () => (
-	<Suspense fallback={<p>loading...</p>}>
-		<Switch>
-			<Route path="/members/:id" exact component={EmployeeProfile} />
-			<Route path="/teams/:id" exact component={Team} />
-			<Route path="/teams" exact component={Teams} />
-			<Route path="/" exact component={NotFound404} />
-			<Route path="*" exact component={NotFound404} />
-		</Switch>
-	</Suspense>
+	<Switch>
+		<Route path="/items" exact component={Items} />
+		<Route path="/" exact component={Items} />
+		<Route path="*" exact component={Items} />
+	</Switch>
 );
 
 export default Routes;
