a9d14f7df9a92512fa160e75351f4b6f889a1d1b
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 11:47:02 -0300

    [master] feat: Basic ItemsList component

--- 1 - modules with entities and use-cases/src/domains/Items/components/ItemsList/ItemsList.js
+++ 1 - modules with entities and use-cases/src/domains/Items/components/ItemsList/ItemsList.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const ItemsList = () => {
-	const aaa = '';
+// Css
+import * as s from './ItemsList.style';
 
-	return (
-		<>{aaa}</>
-	);
+// Mock
+import items from './__mocks__/items';
+
+const ItemsList = () => {
+	function addItem() {
+		return '';
+	}
+
+	return items.map(item => (
+		<s.Wrapper>
+			<s.ItemButton
+				type="button"
+				onClick={addItem}
+			>
+				{item.name}
+			</s.ItemButton>
+		</s.Wrapper>
+	));
 };
 
 export default ItemsList;
