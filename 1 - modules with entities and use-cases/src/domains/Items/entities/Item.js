4ec4a7d459d21dbd65fd0357e03657770b5fea96
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 15:36:17 -0300

    [master] feat: Add funcs to entitie

--- 1 - modules with entities and use-cases/src/domains/Items/entities/Item.js
+++ 1 - modules with entities and use-cases/src/domains/Items/entities/Item.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
-const Item = ({
+const makeItem = ({
 	id = null,
 	name = '',
 	quantity = 0,
-	unity,
+	unity = '',
 	price = 0,
 	discount = 0,
 	observations = '',
@@ -13,14 +13,19 @@ const Item = ({
 	created_at,
 	updated_at,
 } = {}) => {
+	const errors = [];
+
 	if (!id) {
-		throw new Error('Item must have an id');
+		// throw new Error('Item must have an id');
+		errors.push('Item must have an id');
 	}
 	if (!name) {
-		throw new Error('Item must have a name');
+		// throw new Error('Item must have a name');
+		errors.push('Item must have a name');
 	}
 	if (!order_id) {
-		throw new Error('Item must belong to an order');
+		// throw new Error('Item must belong to an order');
+		errors.push('Item must belong to an order');
 	}
 
 	function emptyQuantity() {
@@ -31,12 +36,32 @@ const Item = ({
 		return (quantity * price) - discount;
 	}
 
+	function fullItem() {
+		return {
+			id,
+			name,
+			quantity,
+			unity,
+			price,
+			discount,
+			observations,
+			properties,
+			order_id,
+			status,
+			created_at,
+			updated_at,
+		};
+	}
+
 	return Object.freeze({
 		getName: () => name,
 		getUnity: () => unity,
 		getPrice: () => getPrice(),
 		emptyQuantity: () => emptyQuantity(),
+		fullItem: () => fullItem(),
+		hasErrors: () => errors.length > 0,
+		errors: () => errors,
 	});
 };
 
-export default Item;
+export default makeItem;
