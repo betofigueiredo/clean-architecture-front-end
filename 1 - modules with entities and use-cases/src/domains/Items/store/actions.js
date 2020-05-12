f4776ab1be6f91e6c119d3eed623f13c7130dc34
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 15:36:43 -0300

    [master] feat: Redux action to add item

--- 1 - modules with entities and use-cases/src/domains/Items/store/actions.js
+++ 1 - modules with entities and use-cases/src/domains/Items/store/actions.js
@@ -1,13 +1,4 @@
-export const loginRequested = ({ email, senha }) => ({
-	type: '[auth] LOGIN_REQUESTED',
-	email,
-	senha,
-});
-
-export const authRequested = () => ({
-	type: '[auth] AUTH_REQUESTED',
-});
-
-export const clearAuth = () => ({
-	type: '[auth] AUTH_CLEARED',
+export const itemAdded = ({ item }) => ({
+	type: '[items] ITEM_ADDED',
+	item,
 });
