83053b3a4ea349249901ba8e6834362a71f7efc1
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 15:35:51 -0300

    [master] feat: Clear ItemsList component

--- 1 - modules with entities and use-cases/src/domains/Items/components/ItemsList/ItemsList.js
+++ 1 - modules with entities and use-cases/src/domains/Items/components/ItemsList/ItemsList.js
@@ -3,24 +3,21 @@ import React from 'react';
 // Css
 import * as s from './ItemsList.style';
 
+// Components
+import ItemRow from '../ItemRow';
+
 // Mock
 import items from './__mocks__/items';
 
-const ItemsList = () => {
-	function addItem() {
-		return '';
-	}
-
-	return items.map(item => (
-		<s.Wrapper>
-			<s.ItemButton
-				type="button"
-				onClick={addItem}
-			>
-				{item.name}
-			</s.ItemButton>
-		</s.Wrapper>
-	));
-};
+const ItemsList = () => (
+	<s.Wrapper>
+		{items.map(item => (
+			<ItemRow
+				key={item.id}
+				item={item}
+			/>
+		))}
+	</s.Wrapper>
+);
 
 export default ItemsList;
