772d6b88007c386005bca0d4ec863bf85663be27
Beto Figueiredo <herbert_f@yahoo.com.br>
2020-05-12 15:38:07 -0300

    [master] feat: Use case to add item

---
+++ 1 - modules with entities and use-cases/src/domains/Items/use_cases/itemAdded/itemAdded.js
@@ -0,0 +1,28 @@
+import makeItem from '../../entities';
+
+const itemAdded = (state = {}, action = {}) => {
+	const item_to_add = action.item || {};
+
+	function addItemToState(item) {
+		return {
+			...state,
+			item_on_edition: item.fullItem(),
+		};
+	}
+
+	function checkErrors(item) {
+		if (item.hasErrors()) {
+			throw new Error('Item has errors');
+		}
+		return addItemToState(item);
+	}
+
+	function initItem() {
+		const item = makeItem({ ...item_to_add });
+		return checkErrors(item);
+	}
+
+	return initItem();
+};
+
+export default itemAdded;
