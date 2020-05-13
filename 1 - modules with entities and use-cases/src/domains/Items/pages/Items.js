51fec3b6734c2d75c95bf3bc24907d10c48daf9a
betofigueiredo <herbert_f@yahoo.com.br>
2020-05-12 21:27:19 -0300

    [master] feat: Itemm page component

--- 1 - modules with entities and use-cases/src/domains/Items/pages/Items.js
+++ 1 - modules with entities and use-cases/src/domains/Items/pages/Items.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 
 // Components
 import ItemsList from '../components/ItemsList';
 import ItemEdition from '../components/ItemEdition';
 
-const Items = () => (
-	<>
-		<ItemsList />
-		<ItemEdition />
-	</>
-);
+const Items = () => {
+	const item_on_edition_id = useSelector(store => store
+		.items?.item_on_edition?.id, shallowEqual) || null;
+	const has_item_to_edit = item_on_edition_id !== null;
+
+	return has_item_to_edit
+		? <ItemEdition />
+		: <ItemsList />;
+};
 
 export default Items;
