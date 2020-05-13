41e0d0151082ed960274d83da5125523777e8641
betofigueiredo <herberto_f@yahoo.com.br>
2020-05-12 21:43:21 -0300

    [master] feat: Style file

---
+++ 1 - modules with entities and use-cases/src/domains/Items/components/ItemEditionQuantity/ItemEditionQuantity.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    position: relative;
+    padding: 40px 0;
+`;
+
+export const ItemButton = styled.button`
+    position: relative;
+`;
